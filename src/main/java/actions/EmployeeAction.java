c17a3d2bdf72b004235be8209010a1f4772ad51c
toyotoshi.fukuyama <tfukuyama0824@gmail.com>
2021-11-16 09:59:05 +0900

    従業員newアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -38,7 +38,14 @@ public class EmployeeAction extends ActionBase {
      * @throws ServletException
      * @throws IOException
      */
-    public void index() throws ServletException, IOException {
+    public void entryNew() throws ServletException, IOException {
+
+
+        putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
+        putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView()); //空の従業員インスタンス
+
+        //新規登録画面を表示
+        forward(ForwardConst.FW_EMP_NEW);
 
         //指定されたページ数の一覧画面に表示するデータを取得
         int page = getPage();
